7bd00d126c0bbf2fd126eecbbcf24f8a212d6fa0
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-08-26 17:07:16 -0500

    updated booklist exercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,26 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let greatMysteries = "Great Mystery Books";
+  let book1 =
+    "https://m.media-amazon.com/images/I/81FiMxOzzaL._AC_UF1000,1000_QL80_.jpg";
+  let book2 =
+    "https://m.media-amazon.com/images/I/71ov8-gJKaL._AC_UF1000,1000_QL80_.jpg";
+  let book3 = "https://pictures.abebooks.com/isbn/9780590463959-us.jpg";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{greatMysteries}</h3>
+      <img
+        src={book1}
+        alt="Image of the Still Life book cover by author Louise Penny. Contains a vase with flowers sitting on a table in front of a glass window"
+      />
+      <img
+        src={book2}
+        alt="Image of the cover for the book The Sweetness at the Bottom of the Pie by Alan Bradley. Includes a dead crow with a postage stamp through its beak."
+      />
+      <img
+        src={book3}
+        alt="Image of cover of Nancy Drew the Secret of the Old Clock. Nancy is kneeling in a field holding a screwdriver and an old mantel clock."
+      />
+    </div>
+  );
 }
