773a02b9790229875f17ef459fc7201a69f9a9fe
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-08-31 23:37:45 -0500

    updated chores list file

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,24 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from "react";
+import classes from "./ChoresList.module.css";
+
+export default function ChoresList() {
+  const chores = [
+    "Clean floors",
+    "Feed dog",
+    "Mow yard",
+    "Water plants",
+    "Take out trash",
+  ];
+  return (
+    <div>
+      <h3 className={classes.choresHeading}>List of Chores to Accomplish</h3>
+      <ol>
+        <li className={classes.choresText}>{chores[0]}</li>
+        <li className={classes.choresText}>{chores[1]}</li>
+        <li className={classes.choresText}>{chores[2]}</li>
+        <li className={classes.choresText}>{chores[3]}</li>
+        <li className={classes.choresText}>{chores[4]}</li>
+      </ol>
+    </div>
+  );
 }
