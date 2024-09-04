baa23dc44c52ba54ad369c6a2f9c804c0708f55d
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-09-04 13:42:18 -0500

    Introduction file complete

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,12 +1,24 @@
-export default function HobbyIntroduction () {
-   return(
-      <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
-         <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
-         </ol>
-      </div>
-   );
+export default function HobbyIntroduction() {
+  return (
+    <div>
+      <h2>3 Fun Facts About Hiking</h2>
+      <ol>
+        <li>
+          "Missouri has 93 state parks and historic sites, which includes trails
+          and access to the Ozark Trail for backpacking. The Missouri Department
+          of Natural Resources manages the state park system, which includes
+          over 150,000 acres of public land." (MO Dept of Natural Resources)
+        </li>
+        <li>
+          Hiking is a full-body workout that can improve mental health, heart
+          health and balance. "A small number of studies have measured
+          antioxidant levels in the blood after forest bathing and found that
+          helpful antioxidants increased significantly." (Psychology Today)
+        </li>
+        <li>
+          HIking is great exercise for dogs (especially Boxer dogs named Beau).
+        </li>
+      </ol>
+    </div>
+  );
 }
